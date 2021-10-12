a07a645fbd0e11d0b50d34ca49f0a394db3ea5a7
SwarnaliS <90927960+SwarnaliS@users.noreply.github.com>
2021-10-12 16:52:43 -0600

    Added Project3 folder

---
+++ Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,45 @@
+
+//Addition
+function addition(x,y){
+    return x+y;
+}
+
+//Subtraction
+function subtraction(x,y){
+    return x-y;
+}
+
+//Multiplication
+function multiplication(x,y){
+    return x*y;
+}
+
+//multiple operations
+function multiple(x,y,z,t){
+    return x*y/z+t;
+}
+
+//Modulus
+function modulus(x,y){
+    return x%y;
+}
+
+//negation
+function negation_Operator(x){
+    return x;
+}
+
+//Increment and Decrement
+var o=8;
+o++;
+document.write(o);
+
+var p=8;
+p--;
+document.write(p);
+//random number
+window.alert (Math.random()* 100);
+
+
+
+
