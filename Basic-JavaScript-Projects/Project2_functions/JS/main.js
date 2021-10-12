4466c98b1bd96a1a402f56ad54fcc76b79da20fd
SwarnaliS <90927960+SwarnaliS@users.noreply.github.com>
2021-10-12 13:54:25 -0600

    Added Comment line

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -4,8 +4,14 @@
     };
 
     function myFunction(){
-        var sentence="This is the first line";
+        var sentence="This is the first line <br>";
          sentence +="This is the addition to the first line.";
         document.getElementById("Concatenate").innerHTML= sentence;
     };
-
+    //writing function 
+    function addition(x,y){
+        return (x+y);
+    }
+    
+    
+    
