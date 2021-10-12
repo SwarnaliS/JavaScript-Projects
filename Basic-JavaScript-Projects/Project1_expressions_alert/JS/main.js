b9b813aecaf16823d183009988099ec2ae1e57f3
SwarnaliS <90927960+SwarnaliS@users.noreply.github.com>
2021-10-11 18:44:00 -0600

    Added Project1 Expression file

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+
+var Y="Hello, World!";
+window.alert(Y);
+
+var X="This is a string variable";
+document.write(X);
+
+document.write("I said,\"you need to go to school\" ");
+
+document.write("\"Pari is 10 years old"
++" but she doesn\'t go to school" 
++"she loves to play all day long\"");
+
+var Z="Concatinating" + "string";
+document.write(Z);
+
+var Family="The Satpathys", Mom="Diya", Baby="Tintin";
+document.write(Baby);
+
+var blue="We are family";
+var blue=blue.fontcolor ("Blue");
+document.write(blue);
+
+document.write(3+3);
+//checking how to write comment
