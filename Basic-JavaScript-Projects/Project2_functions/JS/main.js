5a52383fb5e22953fa729e04feda9929ecc90ee5
SwarnaliS <90927960+SwarnaliS@users.noreply.github.com>
2021-10-12 12:36:43 -0600

    Adding Project 2 folder

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+
+    function displayDate(){
+    document.getElementById("date").innerHTML= Date()
+    };
+
+    function myFunction(){
+        var sentence="This is the first line";
+         sentence +="This is the addition to the first line.";
+        document.getElementById("Concatenate").innerHTML= sentence;
+    };
+
